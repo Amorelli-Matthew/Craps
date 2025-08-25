3dbaf78794a139f00a56c732de24a03663fc5e1d
Amorelli-Matthew <mattam.business@yahoo.com>
2025-08-24 19:43:10 -0700

    Initial commit

---
+++ inclass1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace inclass1
+{
+    partial class FormCrapsGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRoll = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCrapsGame = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonRoll
+            // 
+            this.buttonRoll.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.buttonRoll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRoll.Location = new System.Drawing.Point(318, 138);
+            this.buttonRoll.Name = "buttonRoll";
+            this.buttonRoll.Size = new System.Drawing.Size(119, 51);
+            this.buttonRoll.TabIndex = 0;
+            this.buttonRoll.Text = "Roll";
+            this.buttonRoll.UseVisualStyleBackColor = false;
+            this.buttonRoll.Click += new System.EventHandler(this.buttonRoll_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 144);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Roll List here";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(489, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 144);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Winner!";
+            // 
+            // labelCrapsGame
+            // 
+            this.labelCrapsGame.AutoSize = true;
+            this.labelCrapsGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCrapsGame.Location = new System.Drawing.Point(200, 21);
+            this.labelCrapsGame.Name = "labelCrapsGame";
+            this.labelCrapsGame.Size = new System.Drawing.Size(399, 73);
+            this.labelCrapsGame.TabIndex = 4;
+            this.labelCrapsGame.Text = "Craps! game";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(318, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormCrapsGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelCrapsGame);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonRoll);
+            this.Name = "FormCrapsGame";
+            this.Text = "Craps! Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRoll;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCrapsGame;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ inclass1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace inclass1
+{
+    // Name: Matthew Amorelli
+
+    // Date: September 21, 2021
+
+    // Assignment: Craps! game (Chapter 4- exersize 3)
+
+    // Description: This is the Craps Game we made in class on September 21, 2021
+    public partial class FormCrapsGame : Form
+    {
+        //declear the varables
+        Random Rand = new Random();
+
+        int playerRoll = 0;
+        bool isFirstRoll = true;
+
+        public FormCrapsGame()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRoll_Click(object sender, EventArgs e)
+        {
+            int dieOne = Rand.Next(1, 6);
+            int dieTwo = Rand.Next(1, 7);
+            int roll = dieOne + dieTwo;
+
+
+            //if first roll and not 2,3, or 12
+            if (isFirstRoll == true)
+            {
+                if (roll < 4 || playerRoll == 12)
+                {
+                    label2.Text = "Loser rolled: " + roll;
+                }
+                else if (roll == 7 || roll == 11)
+                {
+                    label2.Text = "Loser rolled: " + roll;
+                }
+                else
+                {
+                    playerRoll = dieOne + dieTwo;
+                    label2.Text = "player rolled: " + playerRoll;
+                    isFirstRoll = false;
+                }
+            }
+
+            else
+            {
+                if (playerRoll == roll)
+                {
+                    label2.Text += "\r\nPlayer Wins! Rolled " + roll;
+                    isFirstRoll = true;
+                }
+
+                else if (roll < 4 || roll == 7 || roll > 10)
+                {
+                    label2.Text += "\r\nPlayer Loses! Rolled " + roll;
+                    isFirstRoll = true;
+                }
+                else
+                {
+                    label2.Text += "\r\nPlayer Wins! Rolled " + roll + " Roll Again...";
+                }
+
+
+                int One = 1;
+                One = One + 7;
+
+                label1.Text = One.ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
